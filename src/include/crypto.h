48168cb9ac438efc0cb04c3fe78edd9840212dd2
PolynomialDivision <vincent@systemli.org>
2017-08-20 17:50:11 +0200

    Change crypto to ssl

--- src/include/crypto.h
+++ src/include/crypto.h
@@ -3,11 +3,17 @@
 
 #include <stdlib.h>
 
+char *base_64(const unsigned char *input, int length);
+
+char *unbase_64(unsigned char *input, int length);
+
 void gcrypt_init();
 
 void gcrypt_set_key_and_iv(char *key, char *iv);
 
-char *gcrypt_encrypt_msg(char *msg, size_t msg_length);
+//char *gcrypt_encrypt_msg(char *msg, size_t msg_length);
+char *gcrypt_encrypt_msg(char *msg, size_t msg_length, int* out_length);
+
 
 char *gcrypt_decrypt_msg(char *msg, size_t msg_length);
 
@@ -23,6 +29,11 @@ unsigned char *base64_decode(const char *data,
 void build_decoding_table();
 
 void base64_cleanup();
+
+int Base64decode_len(const char *bufcoded);
+int Base64encode_len(int len);
+int Base64encode(char *encoded, const char *string, int len);
+int Base64decode(char *bufplain, const char *bufcoded);
 
 
 #endif //DAWN_CRYPTO_H
--- src/network/networksocket.c
+++ src/network/networksocket.c
@@ -140,11 +140,12 @@ void *receive_msg_enc(void *args) {
         }
         //recv_string[recv_string_len] = '\0';
 
-        // first decode base 64
+        char* base64_dec_str = malloc(Base64decode_len(recv_string));
+        int base64_dec_length = Base64decode(base64_dec_str, recv_string);
 
-        size_t base64_msg_len;
-        unsigned char* base64_msg_dec = base64_decode(recv_string, recv_string_len, &base64_msg_len);
-        char *dec = gcrypt_decrypt_msg((char*)base64_msg_dec, base64_msg_len);
+        char *dec = gcrypt_decrypt_msg(base64_dec_str, base64_dec_length);
+
+        free(base64_dec_str);
 
         printf("[WC] Network-Received: %s\n", dec);
 
@@ -207,16 +208,15 @@ int send_string_enc(char *msg) {
     pthread_mutex_lock(&send_mutex);
     size_t msglen = strlen(msg);
 
-    char *enc = gcrypt_encrypt_msg(msg, msglen + 1);
+    int length_enc;
+    char *enc = gcrypt_encrypt_msg(msg, msglen + 1, &length_enc);
 
-    //char* base64_msg_dec = g_base64_encode(msg, msglen + 1);
-
-    size_t base64_msg_len;
-    char* base64_msg_dec = base64_encode((unsigned char*)enc, msglen, &base64_msg_len);
+    char* base64_enc_str = malloc(Base64encode_len(length_enc));
+    size_t base64_enc_length = Base64encode(base64_enc_str, enc, length_enc);
 
     if (sendto(sock,
-               base64_msg_dec,
-               msglen + 1, // very important to use actual length of string because of '\0' in encrypted msg
+               base64_enc_str,
+               base64_enc_length, // very important to use actual length of string because of '\0' in encrypted msg
                0,
                (struct sockaddr *) &addr,
                sizeof(addr)) < 0) {
@@ -224,6 +224,8 @@ int send_string_enc(char *msg) {
         pthread_mutex_unlock(&send_mutex);
         exit(EXIT_FAILURE);
     }
+    free(base64_enc_str);
+    free(enc);
     pthread_mutex_unlock(&send_mutex);
     return 0;
 }
