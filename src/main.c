c89e57bf7ef43c7b47113d00671a62f253d5ac20
PolynomialDivision <vincent@systemli.org>
2017-08-25 15:05:56 +0200

    Try to disable clients

--- src/main.c
+++ src/main.c
@@ -87,8 +87,8 @@ int main(int argc, char **argv) {
     //pthread_t tid_client;
     //pthread_create(&tid_client, NULL, &remove_client_array_thread, NULL);
 
-    pthread_t tid_get_client;
-    pthread_create(&tid_get_client, NULL, &update_clients_thread, NULL);
+    //pthread_t tid_get_client;
+    //pthread_create(&tid_get_client, NULL, &update_clients_thread, NULL);
 
     pthread_t tid_kick_clients;
     pthread_create(&tid_kick_clients, NULL, &kick_clients_thread, NULL);
