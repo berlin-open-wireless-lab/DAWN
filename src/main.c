fc48e1c1db3e013e531b83c443b5f54691550ec1
PolynomialDivision <vincent@systemli.org>
2018-02-01 12:53:07 +0100

    add prints

--- src/main.c
+++ src/main.c
@@ -86,6 +86,7 @@ int init_mutex() {
 
 int main(int argc, char **argv) {
 
+    printf("DAAAAAAAAAAAAAAAAAAAAAWWWWWWWWWWWWWWWWNNNNNNN\n");
     printf("\nDAWN sizeof(probe_entry_s) == %d\n", sizeof(struct probe_entry_s));
 
     const char *ubus_socket = NULL;
