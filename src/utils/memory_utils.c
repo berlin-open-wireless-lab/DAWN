berlin-open-wireless-lab/DAWN

commit 850a75c18293fef2586513704142728fa0eba712

fix compilation with GCC12
Signed-off-by: Rosen Penev <rosenp@gmail.com>

src/utils/memory_utils.c
@@ -33,7 +33,7 @@ void* ret = NULL;
     case DAWN_REALLOC:
         ret = realloc(ptr, size);
         if (ret != NULL)
-          dawn_memory_unregister(DAWN_REALLOC, file, line, ptr);
+          dawn_memory_unregister(DAWN_REALLOC, file, line, ret);
         break;
     case DAWN_CALLOC:
         ret = calloc(nmemb, size);

src/network/networksocket.c
@@ -70,11 +70,6 @@ void *receive_msg(void *args) {
             continue;
         }
 
-        // TODO: recv_string is a fixed array.  Should test be here?
-        if (recv_string == NULL) {
-            return 0;
-        }
-
         if (strlen(recv_string) <= 0) {
             return 0;
         }
@@ -91,11 +86,6 @@ void *receive_msg_enc(void *args) {
                      recvfrom(sock, recv_string, MAX_RECV_STRING, 0, NULL, 0)) < 0) {
             dawnlog_error("Could not receive message!\n");
             continue;
-        }
-
-        // TODO: recv_string is a fixed array.  Should test be here?
-        if (recv_string == NULL) {
-            return 0;
         }
 
         if (strlen(recv_string) <= 0) {

commit 88c78b4a03dc642665cb4df3428ca93eb235bd1a

memory_utils: fix reallocation
Fixes errors in the form of:
  dawn: mem-audit: Releasing memory we hadn't registered (tcpsocket.c:142)...
  dawn: mem-audit: attempted to register memory already registered (M@tcpsocket.c:114)...

Fixes: 850a75c18293 ("fix compilation with GCC12")

Signed-off-by: Nick Hainke <vincent@systemli.org>

src/utils/memory_utils.c
@@ -33,7 +33,21 @@ void* ret = NULL;
     case DAWN_REALLOC:
         ret = realloc(ptr, size);
         if (ret != NULL)
-          dawn_memory_unregister(DAWN_REALLOC, file, line, ret);
+/*
+GCC v12 has a new Wuse-after-free error. We can not unregister 
+the memory before doing a reallocation. The call can fail, so
+ptr is never freed. However, the warning can be ignored, since
+ptr is only used in the unregister function as a reference to
+remove it from our memory auditing.
+*/
+#if (__GNUC__ >= 12)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wuse-after-free"
+            dawn_memory_unregister(DAWN_REALLOC, file, line, ptr);
+#pragma GCC diagnostic pop
+#else
+            dawn_memory_unregister(DAWN_REALLOC, file, line, ptr);
+#endif
         break;
     case DAWN_CALLOC:
         ret = calloc(nmemb, size);