1cd73579e5dde35b5aa8feee81163f2ea49d9342
g0tar <gotar@polanet.pl>
2026-06-16 17:29:27 +0200

    ubus: suppress BSS transition request with empty NR for static AP list with disabled 802.1k
    update_beacon_reports '0' shouldn't make difference for set_hostapd_nr '1'

--- src/utils/ubus.c
+++ src/utils/ubus.c
@@ -1278,8 +1278,11 @@ int bss_transition_request(uint32_t id, const struct dawn_mac client_addr, struc
     }
 
     blobmsg_close_array(&b, nbs);
-    // do not ask to leave without providing candidates, unless NR explicitly disabled or unavailable (consider empty rrm_mode)
-    if (neighbors_added || (dawn_metric.disassoc_nr_length <= 0) || (dawn_metric.set_hostapd_nr <= 0) || (timeout_config.update_beacon_reports <= 0) ) {
+    // do not ask to leave without providing candidates, unless NRs are explicitly disabled or unavailable
+    if (neighbors_added || 
+            (dawn_metric.disassoc_nr_length <= 0) || (dawn_metric.set_hostapd_nr <= 0) ||
+            // disabling 802.11k shouldn't matter unless we build dynamic NR list (consider empty rrm_mode)
+            ((timeout_config.update_beacon_reports <= 0) && (dawn_metric.set_hostapd_nr == 2)) ) {
         int timeout = 1;
         ubus_invoke(ctx, id, "bss_transition_request", b.head, NULL, NULL, timeout * 1000);
     }
