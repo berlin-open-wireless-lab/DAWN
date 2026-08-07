3eb6d53fbea30f40544677b22a5606854d2e11c3
Michael Pfeifroth <micpf@westermo.com>
2026-08-07 15:14:04 +0200

    dawn_iwinfo: drop local redefinition of IWINFO_BUFSIZE
    IWINFO_BUFSIZE is already defined by libiwinfo's <iwinfo.h>, which is
    included at the top of this file. Redefining it locally with a
    different value (24 KiB vs. 96 KiB in current libiwinfo) causes a
    compiler warning about macro redefinition and, more importantly, means
    the buffers allocated in this file are smaller than what libiwinfo may
    write into them. Rely on the header's definition instead.
    
    Signed-off-by: Michael Pfeifroth <micpf@westermo.com>

--- src/utils/dawn_iwinfo.c
+++ src/utils/dawn_iwinfo.c
@@ -15,8 +15,6 @@ int get_rssi(const char *ifname, struct dawn_mac client_addr);
 
 int get_bandwidth(const char *ifname, struct dawn_mac client_addr, float *rx_rate, float *tx_rate);
 
-#define IWINFO_BUFSIZE    24 * 1024
-
 #define IWINFO_ESSID_MAX_SIZE    32
 
 int compare_essid_iwinfo(struct dawn_mac bssid_addr, struct dawn_mac bssid_addr_to_compare) {
